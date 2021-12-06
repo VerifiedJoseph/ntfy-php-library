af429f28a3d65d2968d7ce527c1f2783f2b0d6a0
Joseph <joseph@verifiedjoseph.com>
2021-12-06 21:20:55 +0000

    Create get-messages.php

---
+++ examples/get-messages.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * 	Code example for gettings sent messages
+ */
+
+include '../vendor/autoload.php';
+
+use Ntfy\Exception\NtfyException;
+use Ntfy\Exception\EndpointException;
+
+try {
+	// Set server
+	$server = new Ntfy\Server('https://ntfy.sh/');
+
+	// Create Ntfy class instance
+	$ntfy = new Ntfy\Ntfy($server);
+		
+	$messages = $ntfy->get(
+		topic: 'myTopic',
+	);
+
+	// Dump sent messages
+	var_dump($messages);
+
+} catch (EndpointException | NtfyException $err) {
+	echo $err->getMessage();
+}
