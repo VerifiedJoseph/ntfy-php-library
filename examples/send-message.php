00488ad9430b9d1704edd118e70c988aff06e8bc
Joseph <joseph@verifiedjoseph.com>
2022-01-31 17:59:47 +0000

    Update send-message.php

--- examples/send-message.php
+++ examples/send-message.php
@@ -6,23 +6,33 @@
 
 include '../vendor/autoload.php';
 
+use Ntfy\Server;
+use Ntfy\Message;
+
 use Ntfy\Exception\NtfyException;
 use Ntfy\Exception\EndpointException;
 
 try {
 	// Set server
-	$server = new Ntfy\Server('https://ntfy.sh/');
+	$server = new Server('https://ntfy.sh/');
+	
+	// Create a new message 
+	$message = new Message($server);
+	$message->topic('mytopic');
+	$message->title('Hello World');
+	$message->body('Hello World from ntfy.sh');
+	$message->priority(Message::PRIORITY_HIGH);
 
-	// Create Ntfy class instance
-	$ntfy = new Ntfy\Ntfy($server);
+	// Sent the message
+	$details = $message->send();
 
-	$sentMessage = $ntfy->send(
-		topic: 'myTopic',
-		message: 'Hello World'
-	);
-
-	// Dump sent message details
-	var_dump($sentMessage);
+	// Display sent message details
+	echo 'Id: ' . $details->id . PHP_EOL;
+	echo 'Time: ' . $details->time . PHP_EOL;
+	echo 'Topic: ' . $details->topic . PHP_EOL;
+	echo 'Title: ' . $details->title . PHP_EOL;
+	echo 'Message: ' . $details->message . PHP_EOL;
+	echo 'Priority: ' . $details->priority . PHP_EOL;
 
 } catch (EndpointException | NtfyException $err) {
 	echo $err->getMessage();
