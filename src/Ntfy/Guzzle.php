12a61e3e453c9c1eca300f8e7fe0c30bb5c6880a
Joseph <joseph@verifiedjoseph.com>
2022-05-04 21:03:23 +0100

    [Guzzle] Remove method putFile()

--- src/Ntfy/Guzzle.php
+++ src/Ntfy/Guzzle.php
@@ -22,7 +22,7 @@ final class Guzzle
 	private Client $client;
 
 	/** @var array<int, string> $requestMethods Array of supported HTTP request methods */
-	private array $requestMethods = array('GET', 'POST', 'PUT');
+	private array $requestMethods = array('GET', 'POST');
 
 	/** @var int $timeout Request timeout in seconds */
 	private int $timeout = 10;
@@ -73,30 +73,6 @@ final class Guzzle
 	}
 
 	/**
-	 * Make PUT request with a file
-	 *
-	 * @param string $endpoint API endpoint
-	 * @param string $filepath file path
-	 * @param array<string, mixed> $headers
-	 * @return ResponseInterface
-	 *
-	 * @throws NtfyException if the file cannot be opened
-	 */
-	public function putFile(string $endpoint, string $filepath, array $headers = array()): ResponseInterface
-	{
-		try {
-			$options = array(
-				RequestOptions::HEADERS => $headers,
-				RequestOptions::BODY => Psr7\Utils::tryFopen($filepath, 'r')
-			);
-
-			return $this->request('PUT', $endpoint, $options);
-		} catch (\RuntimeException $err) {
-			throw new NtfyException($err->getMessage());
-		}
-	}
-
-	/**
 	 * Make HTTP request
 	 *
 	 * @param string $method HTTP request method
@@ -112,7 +88,7 @@ final class Guzzle
 	{
 		try {
 			if (in_array($method, $this->requestMethods) === false) {
-				throw new NtfyException('Request method must be GET, POST or PUT');
+				throw new NtfyException('Request method must be GET or POST');
 			}
 
 			$response = $this->client->request($method, $endpoint, $options);
