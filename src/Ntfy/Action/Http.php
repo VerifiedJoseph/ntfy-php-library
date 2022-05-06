7258aeaf984801357ad4033a3320c2b38fd13634
Joseph <joseph@verifiedjoseph.com>
2022-05-06 23:00:53 +0100

    [Action\Http] Add class

---
+++ src/Ntfy/Action/Http.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Ntfy\Action;
+
+use Ntfy\Action;
+
+/**
+ * Class for creating a http button action
+ * 
+ * @see https://ntfy.sh/docs/publish/#open-websiteapp
+ */
+class HTTP extends Action
+{
+	/** {@inheritDoc} */
+	protected string $type = 'http';
+
+	/** @var string $url HTTP request URL */
+	protected string $url = '';
+
+	/** @var string $method HTTP request method */
+	protected string $method = 'POST';
+
+	/** @var array<string, string> $headers HTTP request headers */
+	protected array $headers = [];
+
+	/** @var string $body HTTP request body */
+	protected string $body = '';
+
+	/**
+	 * Set HTTP request URL
+	 * 
+	 * @param string $url URL
+	 */
+	public function url(string $url): void
+	{
+		$this->url = $url;
+	}
+
+	/**
+	 * Set HTTP request method
+	 * 
+	 * @param string $method HTTP request method
+	 */
+	public function method(string $method): void
+	{
+		$this->method = $method;
+	}
+
+	/**
+	 * Set an HTTP request header
+	 * 
+	 * @param string $name Header name
+	 * @param string $value Header value
+	 */
+	public function header(string $name, string $value): void
+	{
+		$this->headers[$name] = $value;
+	}
+
+	/**
+	 * Set HTTP request body
+	 * 
+	 * @param string $body Request body
+	 */
+	public function body(string $body): void
+	{
+		$this->body = $body;
+	}
+
+	/** 
+	 * {@inheritDoc}
+	 */
+	protected function generate(): array
+	{
+		$action = parent::generate();
+		$action['url'] = $this->url;
+		$action['method'] = $this->method;
+
+		if ($this->headers !== []) {
+			$action['headers'] = $this->headers;
+		}
+
+		if ($this->body !== '') {
+			$action['body'] = $this->body;
+		}
+
+		return $action;
+	}
+}
