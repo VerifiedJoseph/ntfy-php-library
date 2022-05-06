176d4e9e6a907ab0c3c0ef0c7a114aa7d70aa941
Joseph <joseph@verifiedjoseph.com>
2022-05-06 23:00:15 +0100

    [Action\View] Add class

---
+++ src/Ntfy/Action/View.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ntfy\Action;
+
+use Ntfy\Action;
+
+/**
+ * Class for creating a view button action
+ * 
+ * @see https://ntfy.sh/docs/publish/#open-websiteapp
+ */
+class View extends Action
+{
+	/** {@inheritDoc} */
+	protected string $type = 'view';
+
+	/** @var string $url URL */
+	protected string $url = '';
+
+	/**
+	 * Set action URL
+	 * 
+	 * @param string $url URL
+	 */
+	public function url(string $url): void
+	{
+		$this->url = $url;
+	}
+
+	/** 
+	 * {@inheritDoc}
+	 */
+	protected function generate(): array
+	{
+		$action = parent::generate();
+		$action['url'] = $this->url;
+
+		return $action;
+	}
+}
