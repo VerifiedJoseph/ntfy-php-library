5dc253a5efc6c0a56ad41f49a509123ee0684125
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-10-04 01:42:56 +0100

    Remove unused properties from class MessageTest

--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -2,42 +2,6 @@
 
 class MessageTest extends TestCase
 {
-	/** @var string $topic Message topic */
-	private string $topic = 'mytopic';
-
-	/** @var string $title Message title */
-	private string $title = 'Hello World';
-
-	/** @var string $body Message body */
-	private string $body = 'Hello World from PHPUnit';
-
-	/** @var int $priority Message priority */
-	private int $priority = 4;
-
-	/** @var array<int, string> $tags Message tags */
-	private array $tags = ['hello', 'world'];
-
-	/** @var string $priority Message icon */
-	private string $icon = 'https://ntfy.sh/static/img/ntfy.png';
-
-	/** @var string $attachmentUrl File attachment URL */
-	private string $attachmentUrl = 'https://example.com/index.html';
-
-	/** @var string $attachmentName File attachment name */
-	private string $attachmentName = 'index.html';
-
-	/** @var string $actionType Action type */
-	private string $actionType = 'view';
-
-	/** @var string $actionLabel Action label */
-	private string $actionLabel = 'Open Website';
-
-	/** @var string $actionUrl Action URL */
-	private string $actionUrl = 'https://example.com';
-
-	/** @var bool $actionNoteClear Action clear status */
-	private bool $actionNoteClear = true;
-
 	/**
 	 * Test sending a message
 	 */
