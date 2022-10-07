a85c82f82b7e048dd301d4fb612d8075e6a248a8
Malachi Soord <inverse.chi@gmail.com>
2022-10-07 12:27:44 +0200

    Add php cs fixer (#110)
    * Add php-cs-fixer linting to CI
    * Fix naming
    * Drop strict
    * Fix lock
    * Fix lock
    * Run php-cs-fixer
    * Remove 'DisallowSpaceIndent' from phpcs.xml
    
    Co-authored-by: Joseph <VerifiedJoseph@users.noreply.github.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,41 +7,41 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
  */
 abstract class TestCase extends BaseTestCase
 {
-	protected static string $ntfyUri = 'http://127.0.0.1:8080/';
-	protected static string $httpBinUri = 'https://httpbin.org/';
+    protected static string $ntfyUri = 'http://127.0.0.1:8080/';
+    protected static string $httpBinUri = 'https://httpbin.org/';
 
-	protected static Ntfy\Server $server;
+    protected static Ntfy\Server $server;
 
-	public static function setUpBeforeClass(): void
-	{
-		self::$server = new Ntfy\Server(self::getNtfyUri());
-	}
+    public static function setUpBeforeClass(): void
+    {
+        self::$server = new Ntfy\Server(self::getNtfyUri());
+    }
 
-	/**
-	 * Returns ntfy server URI
-	 *
-	 * Returns value of `self::$ntfyUri` or environment variable `NTFY_URI` if set.
-	 */
-	protected static function getNtfyUri(): string
-	{
-		if (getenv('NTFY_URI') !== false) {
-			return getenv('NTFY_URI');
-		}
+    /**
+     * Returns ntfy server URI
+     *
+     * Returns value of `self::$ntfyUri` or environment variable `NTFY_URI` if set.
+     */
+    protected static function getNtfyUri(): string
+    {
+        if (getenv('NTFY_URI') !== false) {
+            return getenv('NTFY_URI');
+        }
 
-		return self::$ntfyUri;
-	}
+        return self::$ntfyUri;
+    }
 
-	/**
-	 * Returns httpbin server URI
-	 *
-	 * Returns value of `self::$httpBinUri` or environment variable `HTTPBIN_URI` if set.
-	 */
-	protected static function getHttpBinUri(): string
-	{
-		if (getenv('HTTPBIN_URI') !== false) {
-			return getenv('HTTPBIN_URI');
-		}
+    /**
+     * Returns httpbin server URI
+     *
+     * Returns value of `self::$httpBinUri` or environment variable `HTTPBIN_URI` if set.
+     */
+    protected static function getHttpBinUri(): string
+    {
+        if (getenv('HTTPBIN_URI') !== false) {
+            return getenv('HTTPBIN_URI');
+        }
 
-		return self::$httpBinUri;
-	}
+        return self::$httpBinUri;
+    }
 }
