778551c86c65711e837f35a43486d2782e0c3adf
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-11-12 23:01:48 +0000

    Run php-cs-fixer on examples

--- examples/send-message.php
+++ examples/send-message.php
@@ -14,27 +14,26 @@ use Ntfy\Exception\NtfyException;
 use Ntfy\Exception\EndpointException;
 
 try {
-	// Set server
-	$server = new Server('https://ntfy.sh/');
+    // Set server
+    $server = new Server('https://ntfy.sh/');
 
-	// Create a new message
-	$message = new Message();
-	$message->topic('mytopic');
-	$message->title('Hello World');
-	$message->body('Hello World from ntfy.sh');
-	$message->priority(Message::PRIORITY_HIGH);
+    // Create a new message
+    $message = new Message();
+    $message->topic('mytopic');
+    $message->title('Hello World');
+    $message->body('Hello World from ntfy.sh');
+    $message->priority(Message::PRIORITY_HIGH);
 
-	$client = new Client($server);
-	$response = $client->send($message);
+    $client = new Client($server);
+    $response = $client->send($message);
 
-	// Display sent message details
-	echo 'Id: ' . $response->id . PHP_EOL;
-	echo 'Time: ' . $response->time . PHP_EOL;
-	echo 'Topic: ' . $response->topic . PHP_EOL;
-	echo 'Title: ' . $response->title . PHP_EOL;
-	echo 'Message: ' . $response->message . PHP_EOL;
-	echo 'Priority: ' . $response->priority . PHP_EOL;
-
+    // Display sent message details
+    echo 'Id: ' . $response->id . PHP_EOL;
+    echo 'Time: ' . $response->time . PHP_EOL;
+    echo 'Topic: ' . $response->topic . PHP_EOL;
+    echo 'Title: ' . $response->title . PHP_EOL;
+    echo 'Message: ' . $response->message . PHP_EOL;
+    echo 'Priority: ' . $response->priority . PHP_EOL;
 } catch (EndpointException | NtfyException $err) {
-	echo $err->getMessage();
+    echo $err->getMessage();
 }
