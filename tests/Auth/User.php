69513a1ea64d5a9f5eb5bcc9e8e0649af989f2f1
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-02-16 23:47:52 +0000

    tests: Create Auth\User.php

---
+++ tests/Auth/User.php
@@ -0,0 +1,41 @@
+<?php
+
+use Ntfy\Auth\User;
+
+class AuthTest extends TestCase
+{
+    protected static User $auth;
+
+    protected static string $method = 'user';
+    protected static string $username = 'admin';
+    protected static string $password = 'password123';
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$auth = new User(self::$username, self::$password);
+    }
+
+    /**
+     * Test `getMethod()`
+     */
+    public function testGetMethod(): void
+    {
+        $this->assertEquals(self::$method, self::$auth->getMethod());
+    }
+
+    /**
+     * Test `getUsername()`
+     */
+    public function testGetUsername(): void
+    {
+        $this->assertEquals(self::$username, self::$auth->getUsername());
+    }
+
+    /**
+     * Test `getPassword()`
+     */
+    public function testGetPassword(): void
+    {
+        $this->assertEquals(self::$password, self::$auth->getPassword());
+    }
+}
