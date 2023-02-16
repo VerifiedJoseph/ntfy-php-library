4cdedb7cad4d2c08e3d4d6ee52a337f901c9a149
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-02-16 23:48:02 +0000

    tests: Create Auth\Token.php

---
+++ tests/Auth/Token.php
@@ -0,0 +1,32 @@
+<?php
+
+use Ntfy\Auth\Token;
+
+class AuthTest extends TestCase
+{
+    protected static Token $auth;
+
+    protected static string $method = 'token';
+    protected static string $token = 'tk_rHIb9qXgN3b1JJwiT9VVi5tjzHOph';
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$auth = new Token(self::$token);
+    }
+
+    /**
+     * Test `getMethod()`
+     */
+    public function testGetMethod(): void
+    {
+        $this->assertEquals(self::$method, self::$auth->getMethod());
+    }
+
+    /**
+     * Test `getToken()`
+     */
+    public function testGetToken(): void
+    {
+        $this->assertEquals(self::$token, self::$auth->getToken());
+    }
+}
