6d3296d03234706b9ba36f48a20d6a7938fab63c
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-02-17 00:19:56 +0000

    tests: Add testSendWithUserAuth() & testSendWithTokenAuth()

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -9,32 +9,38 @@ use Ntfy\Json;
 
 class ClientTest extends TestCase
 {
+    protected object $authParams;
+    protected object $messageParams;
+    protected object $actionParams;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$authParams = Json::decode(self::loadFixture('auth.json'));
+        self::$messageParams = Json::decode(self::loadFixture('message.json'));
+        self::$actionParams = Json::decode(self::loadFixture('action.json'));
+    }
+
     /**
      * Test sending a message
      */
     public function testSend(): void
     {
-        $messageExample = Json::decode(self::loadFixture('message.json'));
-        $actionExample = Json::decode(self::loadFixture('action.json'));
-
         $server = new Server(self::getNtfyUri());
 
         $action = new Action\View();
-        $action->label($actionExample->label);
-        $action->url($actionExample->url);
+        $action->label(self::$actionParams->label);
+        $action->url(self::$actionParams->url);
         $action->enableNoteClear();
 
         $message = new Message();
-        $message->topic($messageExample->topic);
-        $message->title($messageExample->title);
-        $message->priority($messageExample->priority);
-        $message->body($messageExample->body);
-        $message->tags($messageExample->tags);
+        $message->topic(self::$messageParams->topic);
+        $message->title(self::$messageParams->title);
+        $message->priority(self::$messageParams->priority);
+        $message->body(self::$messageParams->body);
+        $message->tags(self::$messageParams->tags);
         $message->action($action);
 
-        $auth = new Auth('admin', 'password123');
-
-        $client = new Client($server, $auth);
+        $client = new Client($server);
         $response = $client->send($message);
 
         $this->assertTrue(property_exists($response, 'topic'));
@@ -43,16 +49,58 @@ class ClientTest extends TestCase
         $this->assertTrue(property_exists($response, 'priority'));
         $this->assertTrue(property_exists($response, 'tags'));
 
-        $this->assertEquals($messageExample->topic, $response->topic);
-        $this->assertEquals($messageExample->title, $response->title);
-        $this->assertEquals($messageExample->priority, $response->priority);
-        $this->assertEquals($messageExample->body, $response->message);
-        $this->assertEquals($messageExample->tags, $response->tags);
+        $this->assertEquals(self::$messageParams->topic, $response->topic);
+        $this->assertEquals(self::$messageParams->title, $response->title);
+        $this->assertEquals(self::$messageParams->priority, $response->priority);
+        $this->assertEquals(self::$messageParams->body, $response->message);
+        $this->assertEquals(self::$messageParams->tags, $response->tags);
 
         $this->assertCount(1, $response->actions);
-        $this->assertEquals($actionExample->type, $response->actions[0]->action);
-        $this->assertEquals($actionExample->label, $response->actions[0]->label);
-        $this->assertEquals($actionExample->url, $response->actions[0]->url);
-        $this->assertEquals($actionExample->clear, $response->actions[0]->clear);
+        $this->assertEquals(self::$actionParams->type, $response->actions[0]->action);
+        $this->assertEquals(self::$actionParams->label, $response->actions[0]->label);
+        $this->assertEquals(self::$actionParams->url, $response->actions[0]->url);
+        $this->assertEquals(self::$actionParams->clear, $response->actions[0]->clear);
+    }
+
+    /**
+     * Test sending a message with username password auth
+     */
+    public function testSendWithUserAuth(): void
+    {
+        $server = new Server(self::getNtfyUri());
+
+        $message = new Message();
+        $message->topic(self::$messageParams->topic);
+        $message->title(self::$messageParams->title);
+        $message->body(self::$messageParams->body);
+
+        $auth = new Auth\User(
+            self::$authParams->username,
+            self::$authParams->password
+        );
+
+        $client = new Client($server, $auth);
+        $client->send($message);
+    }
+
+    /**
+     * Test sending a message with access token auth
+     */
+    public function testSendWithTokenAuth(): void
+    {
+        $server = new Server(self::getNtfyUri());
+
+        $message = new Message();
+        $message->topic(self::$messageParams->topic);
+        $message->title(self::$messageParams->title);
+        $message->body(self::$messageParams->body);
+
+        $auth = new Auth\User(
+            self::$authParams->username,
+            self::$authParams->password
+        );
+
+        $client = new Client($server, $auth);
+        $client->send($message);
     }
 }
